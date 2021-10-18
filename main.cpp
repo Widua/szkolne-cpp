86e1083dffd1bae1c63ec4e80c1f485c52de49dc
Widua <mikolaj.widla@gmail.com>
2021-10-18 18:20:06 +0200

    Dodano algorytmy na: * Wyszukiwanie wzorcow w tekscie * Szyfrowanie plotkowe * Szyfrowanie kolumnowe (wiem, ze nie powinno sie nie powinno sie inicjowac tabeli ze zmiennej, ale jest to robione na potrzebny szkolne, i to ma po prostu dzialac)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include<bits/stdc++.h>
 
 std::string toLowerCase(std::string word){
 
@@ -48,14 +48,163 @@ bool isAnagram( std::string word1 , std::string word2){
    return true;
 }
 
-void find ( std::string text , std::string expressionToFind , int &n , int coord[] ){
-
-    
-
-
-
+bool find(std::string s, std::string wzorzec, int T[], int& n)
+{
+    int dlT = s.size(), dlW = wzorzec.size(), j;
+    if (dlW > dlT) return false;
+    n = 0;
+    for (int i = 0; i < dlT-dlW+1; i++)
+    {
+        for(j=i; j<i+dlW; j++)
+            if (s[j] != wzorzec[j - i]) break;
+        if (j == i + dlW)
+        {
+            T[n] = i;
+            n++;
+        }
+    }
+    if (n == 0) return false;
+    return true;
 }
 
+void szyfrowaniePlotkowe()
+{
+    int t,n,m,i,j,k,sum=0;
+    std::string s;
+    std::cout<<"Podaj wiadomosc: "<<'\n';
+    std::cin>>s;
+    std::cout<<"Podaj klucz"<<'\n';
+    std::cin>>n;
+    std::vector<std::vector<char>> a(n,std::vector<char>(s.size(),' '));
+    j=0;
+    int flag=0;
+    for(i=0;i<s.size();i++){
+        a[j][i] = s[i];
+        if(j==n-1){
+            flag=1;
+        }
+        else if(j==0)
+            flag=0;
+        if(flag==0){
+            j++;
+        }
+        else j--;
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<s.size();j++){
+            if(a[i][j]!=' ')
+                std::cout<<a[i][j];
+        }
+    }
+    std::cout<<'\n';
+}
+void deszyfracjaPlotkowa()
+{
+    int t,n,m,i,j,k,sum=0;
+    std::string s;
+    std::cout<<"Podaj wiadomosc do rozszyfrowania"<<'\n';
+    std::cin>>s;
+    std::cout<<"Podaj klucz"<<'\n';
+    std::cin>>n;
+    std::vector<std::vector<char>> a(n,std::vector<char>(s.size(),' '));
+    j=0;
+    int flag=0;
+    for(i=0;i<s.size();i++){
+        a[j][i] = '0';
+        if(j==n-1){
+            flag=1;
+        }
+        else if(j==0)
+            flag=0;
+        if(flag==0){
+            j++;
+        }
+        else j--;
+    }
+    int temp =0;
+    for(i=0;i<n;i++){
+        for(j=0;j<s.size();j++){
+            if(a[i][j]=='0')
+                a[i][j]= s[temp++];
+        }
+    }
+    flag=0;
+    j=0;
+    for(i=0;i<s.size();i++){
+        std::cout<<a[j][i];
+        if(j==n-1){
+            flag=1;
+        }
+        else if(j==0)
+            flag=0;
+        if(flag==0){
+            j++;
+        }
+        else j--;
+    }
+    std::cout<<'\n';
+}
+
+std::string odszyfrowanieKolumnowe (std::string wyraz, int n, int klucz[])
+{
+    int wiersze{3}, k{0};
+    if      (wyraz.size()%n==0) wiersze=wyraz.size()/n;
+    else if (wyraz.size()%n!=0) wiersze=wyraz.size()/n+1;
+    char tab[wiersze][n];
+    for (int j=0; j<n; j++)
+    {
+        for (int i=0; i<wiersze&&k<wyraz.size(); i++, k++)
+        {
+            tab[i][j] = wyraz[k];
+        }
+    }
+    for (int i=0; i<wiersze; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            std::cout<<tab[i][j]<<'\t';
+        }
+        std::cout<<"\n";
+    }
+    std::string wynik{""};
+    for (int i=0; i<wiersze; i++)
+    {
+        if (tab[i][klucz[0]]!='-') wynik+=tab[i][2];
+        if (tab[i][klucz[1]]!='-') wynik+=tab[i][1];
+        if (tab[i][klucz[2]]!='-') wynik+=tab[i][0];
+    }
+    return wynik;
+}
+std::string szyfrowanieKolumnowe (std::string wyraz, int n, int klucz[]) {
+    int wiersze, k{0};
+    if (wyraz.size() % n == 0) wiersze = wyraz.size() / n;
+    else if (wyraz.size() % n != 0) wiersze = wyraz.size() / n + 1;
+    char tab[wiersze][n];
+    for (int i = 0; i < wiersze; i++) {
+        for (int j = 0; j < n; j++) {
+            tab[i][j] = '-';
+        }
+    }
+    for (int i = 0; i < wiersze; i++) {
+        for (int j = 0; j < n && k < wyraz.size(); j++, k++) {
+            tab[i][j] = wyraz[k];
+        }
+    }
+    for (int i = 0; i < wiersze; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << tab[i][j] << '\t';
+        }
+        std::cout << "\n";
+    }
+    std::string wynik{""};
+    for (int i = 0; i < n; i++) {
+        int k = klucz[i];
+        for (int j = 0; j < wiersze; j++) {
+            wynik += tab[j][k];
+        }
+    }
+    return wynik;
+}
 
 int main() {
 
@@ -63,6 +212,8 @@ int main() {
     std::cout<<" [1] Sprawdzanie czy slowo nie jest palindromem \n" ;
     std::cout<<" [2] Sprawdzenie czy slowa nie sa anagramami \n" ;
     std::cout<<" [3] Wyszukanie wzorcow w tekscie \n" ;
+    std::cout<<" [4] Szyfrowanie i deszyfrowanie plotkowe \n";
+    std::cout<<" [5] Szyfrowanie i deszyfrowanie kolumnowe \n" ;
     int wybor ;
     std::cin >> wybor ;
 
@@ -93,9 +244,51 @@ int main() {
             } else{
                 printf("%s i %s to nie sa anagramy", word1.c_str() , word2.c_str()) ;
             }
+            break;
         }
         case 3:{
 
+            std::string s, wzorzec;
+            int n, T[256] = { 0 };
+            std::cout << "podaj przeszukiwany tekst: ";
+            std::cin >> s;
+            std::cout << "podaj wzorzc: ";
+            std::cin >> wzorzec;
+            if (find(s, wzorzec, T, n))
+            {
+                std::cout << "liczba wystapien wzorca: " << n << std::endl;
+                std::cout << "podaj wystepowanie wzorca: " << std::endl;
+                for (int i = 0; i < n; i++) std::cout << T[i] << "\t";
+                std::cout << std::endl;
+            }
+            else std::cout<<"\nwzorzec nie wystepuje w tekscie"<<std::endl;
+
+            break;
+        }
+        case 4:{
+
+            szyfrowaniePlotkowe() ;
+
+            deszyfracjaPlotkowa() ;
+
+            break;
+        }
+        case 5:{
+
+            std::string wyraz;
+            int n = 3;
+            int klucz[n] = {2,1,0};
+            std::cout<<"Podaj wyraz ktory chcesz zakodowac: "; std::cin>>wyraz;
+            for (auto & c: wyraz) c = toupper(c);
+
+            std::cout<<szyfrowanieKolumnowe(wyraz, n, klucz);
+
+            std::cout<<"\nPodaj wyraz ktory chcesz odszyfrowac: "; std::cin>>wyraz;
+            for (auto & c: wyraz) c = toupper(c);
+
+            std::cout<<odszyfrowanieKolumnowe(wyraz, n, klucz);
+
+            break;
         }
     }
 
